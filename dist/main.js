c76f49b20b38dc49e56ed3b484cc8274924dccd4
JonathanDaflon <jonathandaflon@yahoo.com>
2020-07-26 10:09:52 -0300

    criar admin por padrão

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const users_mongoose_1 = require("./core/resources/users/users.mongoose");
 const funcionario_controller_1 = require("./core/resources/fullstack-challenge/funcionario.controller");
 const mongodb_1 = require("./core/db/mongodb");
 const users_controller_1 = require("./core/resources/users/users.controller");
@@ -8,5 +9,17 @@ var myServer = config_service_1.configService.CriarServidor();
 if (myServer != null) {
     myServer.Init();
     myServer.ApplyRoutes([users_controller_1.usersController, funcionario_controller_1.funcionarioController]);
-    mongodb_1.mongoConn.creatConnection();
+    mongodb_1.mongoConn.creatConnection().then(() => {
+        users_mongoose_1.User.find({ 'name': 'admin' }).then(r => {
+            if (!r) {
+                let admin = new users_mongoose_1.User({
+                    'name': 'admin',
+                    'email': 'admin@admin',
+                    'password': 'admin',
+                    'gender': 'Male'
+                });
+                admin.save();
+            }
+        });
+    });
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { User } from './core/resources/users/users.mongoose';
 import { funcionarioController } from './core/resources/fullstack-challenge/funcionario.controller';
 import { IServer } from './core/api/api.model';
 import { mongoConn } from './core/db/mongodb';
@@ -11,5 +12,17 @@ if (myServer != null) {
     myServer.Init()
     myServer.ApplyRoutes([usersController, funcionarioController]);
 
-    mongoConn.creatConnection();
+    mongoConn.creatConnection().then(() => {
+        User.find({'name': 'admin'}).then(r => {
+            if (!r) {
+                let admin = new User({
+                    'name': 'admin',
+                    'email': 'admin@admin',
+                    'password': 'admin',
+                    'gender': 'Male'
+                })
+                admin.save();
+            }
+        })
+    });
 }
